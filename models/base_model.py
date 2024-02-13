b0cbb6de9bef4f052bc6f1949a2400fdba868ecf
MahoneyT1 <ifeanyitech15@gmail.com>
2024-02-13 04:23:54 -0800

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,33 @@
-#!/usr/bin/python3
-"""class BaseModel that defines all common attributes
-methods for other classes"""
-
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """the representation of the base class"""
+    """BaseModel class that defines common attributes/methods for other classes."""
 
     def __init__(self):
-        """initializing obj with attribute"""
+        """Initialize BaseModel attributes."""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def save(self):
-        """updates the public instance attribute updated_at with 
-        the current datetime
-        """
+        """Update the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """a dictionary containing all keys/values of __dict__ 
-        of the instance
+        """
+        Return a dictionary containing all keys/values of __dict__ of the instance.
 
         Returns:
-            a dict containing all keys/values of dict
+            dict: Dictionary representation of the instance.
         """
-        obj = {}
-        obj['__class__'] = self.__class__.__name__
-        obj['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        obj['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        obj.update(self.__dict__)
-        return obj
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
-    def __str__(self) -> str:
-        """should print dict representable of class
-
-        Returns:
-            str: representation of a class
-        """
-        return f"[{self.__class__.__name__}] ({self.id} {self.__dict__})"
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
