8c69cef521ad557732cd38f3e2b4f608f93641d8
MahoneyT1 <ifeanyitech15@gmail.com>
2024-02-13 04:11:38 -0800

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""  class BaseModel that defines all common attributes
-methods for other classes """
-
+"""class BaseModel that defines all common attributes
+methods for other classes"""
 
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-    """ the representation of the base class"""
+    """the representation of the base class"""
 
     def __init__(self):
-        """ initializing obj with attribute """
+        """initializing obj with attribute"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -26,19 +26,17 @@ class BaseModel:
         of the instance
 
         Returns:
-            a dict containing all keys / values of dict
+            a dict containing all keys/values of dict
         """
         obj = {}
         obj['__class__'] = self.__class__.__name__
-
-        for key in self.__dict__.keys():
-            obj[key] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-            obj[key] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-            obj.update(self.__dict__)
+        obj['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        obj['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        obj.update(self.__dict__)
         return obj
 
     def __str__(self) -> str:
-        """ should print dict representable of class
+        """should print dict representable of class
 
         Returns:
             str: representation of a class
