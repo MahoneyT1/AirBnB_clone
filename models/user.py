74cd9a83321592158f2b0996a7d068e495b63b4e
MahoneyT1 <ifeanyitech15@gmail.com>
2024-02-16 08:04:02 -0800

    update

--- models/user.py
+++ models/user.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-
+""" user class """
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
+    """ user class that inherits from the basemodel"""
+
     email: str = ""
-    password:str = ""
-    first_name:str = ""
-    last_name:str = ""
+    password: str = ""
+    first_name: str = ""
+    last_name: str = ""
